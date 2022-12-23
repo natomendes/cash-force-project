bc2c6c77167baa823aeffe76d6b5251bb80fc318
Renato Mendes <mendeslrenato@gmail.com>
2022-12-23 17:46:55 -0300

    test: ensure AuthMiddleware returns ok if LoadUserByToken returns a user

--- backend/tests/presentation/middlewares/auth-middleware.spec.ts
+++ backend/tests/presentation/middlewares/auth-middleware.spec.ts
@@ -1,28 +1,33 @@
-import { ValidateToken } from '@/domain/usecases/validate-token'
+import { UserModel } from '@/domain/models/user'
+import { LoadUserByToken } from '@/domain/usecases/load-user-by-token'
 import { AccessDeniedError } from '@/presentation/errors'
-import { forbidden } from '@/presentation/helpers/http-helpers'
+import { forbidden, ok } from '@/presentation/helpers/http-helpers'
 import { AuthMiddleware } from '@/presentation/middlewares'
 
 type SutTypes = {
   sut: AuthMiddleware
-  validateTokenStub: ValidateToken
+  loadUserByTokenStub: LoadUserByToken
 }
 
-const mockValidateToken = (): ValidateToken => {
-  class ValidateTokenStub implements ValidateToken {
-    async validate (token: string): Promise<boolean> {
-      return true
+const mockLoadUserByToken = (): LoadUserByToken => {
+  class LoadUserByTokenStub implements LoadUserByToken {
+    async load (_token: string): Promise<UserModel> {
+      return {
+        id: 1,
+        name: 'any_name',
+        email: 'any_email'
+      }
     }
   }
-  return new ValidateTokenStub()
+  return new LoadUserByTokenStub()
 }
 
 const makeSut = (): SutTypes => {
-  const validateTokenStub = mockValidateToken()
-  const sut = new AuthMiddleware(validateTokenStub)
+  const loadUserByTokenStub = mockLoadUserByToken()
+  const sut = new AuthMiddleware(loadUserByTokenStub)
   return {
     sut,
-    validateTokenStub
+    loadUserByTokenStub
   }
 }
 
@@ -33,25 +38,35 @@ describe('Auth Middleware', () => {
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
   })
 
-  it('Should call ValidateToken with correct value', async () => {
-    const { sut, validateTokenStub } = makeSut()
-    const validateSpy = jest.spyOn(validateTokenStub, 'validate')
+  it('Should call LoadUserByToken with correct value', async () => {
+    const { sut, loadUserByTokenStub } = makeSut()
+    const loadSpy = jest.spyOn(loadUserByTokenStub, 'load')
     await sut.handle({
       headers: {
         'x-access-token': 'any_token'
       }
     })
-    expect(validateSpy).toHaveBeenCalledWith('any_token')
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
 
   it('Should return forbidden if token is invalid or expired', async () => {
-    const { sut, validateTokenStub } = makeSut()
-    jest.spyOn(validateTokenStub, 'validate').mockResolvedValueOnce(false)
+    const { sut, loadUserByTokenStub } = makeSut()
+    jest.spyOn(loadUserByTokenStub, 'load').mockResolvedValueOnce(null)
     const httpResponse = await sut.handle({
       headers: {
         'x-access-token': 'any_token'
       }
     })
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  it('Should return ok if LoadUserByToken returns a user', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    })
+    expect(httpResponse).toEqual(ok({ userId: 1 }))
   })
 })
