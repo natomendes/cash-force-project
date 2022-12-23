0d5824ee7a013f23909d0eab2d09685ceb506427
Renato Mendes <mendeslrenato@gmail.com>
2022-12-23 11:37:17 -0300

    test: ensure DbLoadUserOrders calls LoadUserOrdersRepo with correct value

---
+++ backend/tests/data/usecases/db-load-user-orders.spec.ts
@@ -0,0 +1,36 @@
+import { OrderModel } from '@/domain/models/order'
+import { mockOrderModelList } from '@/tests/helpers/models'
+import { LoadUserOrdersRepo } from '@/data/protocols/load-user-order-repo'
+import { DbLoadUserOrders } from '@/data/usecases/db-load-user-orders'
+
+type SutTypes = {
+  sut: DbLoadUserOrders
+  loadUserOrdersRepoStub: LoadUserOrdersRepo
+}
+
+const mockLoadUserOrdersRepo = (): LoadUserOrdersRepo => {
+  class LoadUserOrdersRepoStub implements LoadUserOrdersRepo {
+    async loadByUserId (_userId: string): Promise<OrderModel[]> {
+      return mockOrderModelList()
+    }
+  }
+  return new LoadUserOrdersRepoStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadUserOrdersRepoStub = mockLoadUserOrdersRepo()
+  const sut = new DbLoadUserOrders(loadUserOrdersRepoStub)
+  return {
+    sut,
+    loadUserOrdersRepoStub
+  }
+}
+
+describe('DbLoadUserOrders Usecase', () => {
+  it('Should call LoadUserOrdersRepo with correct value', async () => {
+    const { sut, loadUserOrdersRepoStub } = makeSut()
+    const loadByUserIdSpy = jest.spyOn(loadUserOrdersRepoStub, 'loadByUserId')
+    await sut.load('valid_user_id')
+    expect(loadByUserIdSpy).toHaveBeenCalledWith('valid_user_id')
+  })
+})
