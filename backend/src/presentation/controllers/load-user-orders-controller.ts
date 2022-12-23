b373b84f106e21b2bfcc3e751fc13e2721527027
Renato Mendes <mendeslrenato@gmail.com>
2022-12-23 01:37:02 -0300

    feat: ensure LoadUserOrdersController returns server error if LoadUserOrders throws

--- backend/src/presentation/controllers/load-user-orders-controller.ts
+++ backend/src/presentation/controllers/load-user-orders-controller.ts
@@ -1,11 +1,20 @@
 import { LoadUserOrders } from '@/domain/usecases/load-user-orders'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { ServerError } from '@/presentation/errors'
 
 export class LoadUserOrdersController implements Controller {
   constructor (private readonly loadUserOrders: LoadUserOrders) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadUserOrders.load(httpRequest.userId)
-    return null
+    try {
+      await this.loadUserOrders.load(httpRequest.userId)
+
+      return null
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
+    }
   }
 }
