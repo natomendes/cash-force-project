f749bcf06612e7375cebd4dab9071f1229c2c5b3
Renato Mendes <mendeslrenato@gmail.com>
2022-12-22 23:06:30 -0300

    feat: add OrderPortion sequelize model

---
+++ backend/src/infra/sequelize/models/OrderPortion.ts
@@ -0,0 +1,38 @@
+import { Model, DataTypes } from 'sequelize'
+import { Order } from './Order'
+import db from '.'
+
+export class OrderPortion extends Model {
+  declare id: number
+  declare nDup: string
+  declare dVenc: string
+  declare vDup: string
+  declare availableToMarket: number
+  declare orderId: number
+  declare createdAt: Date
+  declare updatedAt: Date
+}
+
+OrderPortion.init({
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  nDup: { type: DataTypes.STRING, allowNull: false },
+  dVenc: { type: DataTypes.STRING, allowNull: false },
+  vDup: { type: DataTypes.STRING, allowNull: false },
+  availableToMarket: { type: DataTypes.INTEGER, defaultValue: 1 },
+  createdAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+  updatedAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+  orderId: {
+    type: DataTypes.INTEGER,
+    defaultValue: null,
+    references: {
+      model: 'orders',
+      key: 'id'
+    }
+  }
+}, {
+  sequelize: db,
+  tableName: 'users'
+})
+
+OrderPortion.belongsTo(Order, { foreignKey: 'cnpjId', as: 'cnpj' })
+Order.hasOne(OrderPortion, { foreignKey: 'cnpjId', as: 'cnpj' })
