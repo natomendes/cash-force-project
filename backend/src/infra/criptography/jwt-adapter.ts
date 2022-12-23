d74abe0d857fedcfe844a7f0505b828a52464680
Renato Mendes <mendeslrenato@gmail.com>
2022-12-23 19:22:21 -0300

    feat: ensure JwtAdapter returns null if jwt vefiry throws

--- backend/src/infra/criptography/jwt-adapter.ts
+++ backend/src/infra/criptography/jwt-adapter.ts
@@ -5,8 +5,12 @@ export class JwtAdapter implements Decrypter {
   constructor (private readonly secret: string) {}
 
   async decrypt (token: string): Promise<number> {
-    const { user } = jwt.verify(token, this.secret) as jwt.JwtPayload
+    try {
+      const { user } = jwt.verify(token, this.secret) as jwt.JwtPayload
 
-    return user.id
+      return user.id
+    } catch (error) {
+      return null
+    }
   }
 }
