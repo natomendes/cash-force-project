26493df0d02a771fec8a6f5c4bd7e9f675b83b65
Renato Mendes <mendeslrenato@gmail.com>
2022-12-23 01:48:36 -0300

    feat: ensure LoadUserOrdersController returns a list of orders on success

--- backend/src/presentation/controllers/load-user-orders-controller.ts
+++ backend/src/presentation/controllers/load-user-orders-controller.ts
@@ -1,15 +1,15 @@
 import { LoadUserOrders } from '@/domain/usecases/load-user-orders'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
-import { serverError } from '@/presentation/helpers/http-helpers'
+import { ok, serverError } from '@/presentation/helpers/http-helpers'
 
 export class LoadUserOrdersController implements Controller {
   constructor (private readonly loadUserOrders: LoadUserOrders) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.loadUserOrders.load(httpRequest.userId)
+      const orders = await this.loadUserOrders.load(httpRequest.userId)
 
-      return null
+      return ok(orders)
     } catch (error) {
       return serverError()
     }
--- backend/src/presentation/helpers/http-helpers.ts
+++ backend/src/presentation/helpers/http-helpers.ts
@@ -1,6 +1,11 @@
 import { ServerError } from '@/presentation/errors'
 import { HttpResponse } from '@/presentation/protocols'
 
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
 export const serverError = (): HttpResponse => ({
   statusCode: 500,
   body: new ServerError()
