9f023c5708cd7c4a21baf1aa051c4a76464c2a57
Renato Mendes <mendeslrenato@gmail.com>
2022-12-23 13:59:31 -0300

    feat: ensure request body content is parsed as json

---
+++ backend/src/main/config/app.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+import setupMiddlewares from '@/main/config/middlewares'
+
+const app = express()
+
+setupMiddlewares(app)
+
+export default app
---
+++ backend/src/main/server.ts
@@ -0,0 +1,4 @@
+import 'module-alias/register'
+import app from './config/app'
+
+app.listen(5050, () => console.log('Server running at http://localhost:5050'))
