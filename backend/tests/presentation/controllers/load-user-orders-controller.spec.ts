9e0dcb4e0f2778de0eb7a8e624a4165020ca556a
Renato Mendes <mendeslrenato@gmail.com>
2022-12-23 01:26:28 -0300

    refactor: move usecases mock to its own file

--- backend/tests/presentation/controllers/load-user-orders-controller.spec.ts
+++ backend/tests/presentation/controllers/load-user-orders-controller.spec.ts
@@ -1,23 +1,6 @@
-import { OrderModel } from '@/domain/models/order'
 import { LoadUserOrders } from '@/domain/usecases/load-user-orders'
 import { LoadUserOrdersController } from '@/presentation/controllers/load-user-orders-controller'
-
-const mockLoadUserOrders = (): LoadUserOrders => {
-  class LoadUserOrdersStub implements LoadUserOrders {
-    async load (_userId: string): Promise<OrderModel> {
-      return {
-        id: 1,
-        orderNumber: 'any_order_number',
-        buyer: { id: 1, name: 'any_buyer' },
-        provider: { id: 2, name: 'any_provider' },
-        emissionDate: 'any_date',
-        value: 'any_value',
-        orderStatusBuyer: 'any_status'
-      }
-    }
-  }
-  return new LoadUserOrdersStub()
-}
+import { mockLoadUserOrders } from '../../helpers/usecases'
 
 type SutTypes = {
   sut: LoadUserOrdersController
