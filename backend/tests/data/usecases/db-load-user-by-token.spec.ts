e0577c4a87b64687c8eaa94ce4c17f6e83d55888
Renato Mendes <mendeslrenato@gmail.com>
2022-12-23 18:34:45 -0300

    test: ensure DbLoadUserByToken calls LoadUserById with correct value

--- backend/tests/data/usecases/db-load-user-by-token.spec.ts
+++ backend/tests/data/usecases/db-load-user-by-token.spec.ts
@@ -1,26 +1,44 @@
 import { Decrypter } from '@/data/protocols/decrypter'
 import { DbLoadUserByToken } from '@/data/usecases/db-load-user-by-token'
+import { UserModel } from '@/domain/models/user'
+import { LoadUserByIdRepo } from '@/data/protocols/load-user-by-id-repo'
 
 export const mockDecrypter = (): Decrypter => {
   class DecrypterStub implements Decrypter {
-    async decrypt (_token: string): Promise<string> {
-      return 'decrypted_token'
+    async decrypt (_token: string): Promise<number> {
+      return 1
     }
   }
   return new DecrypterStub()
 }
 
+const mockLoadUserByIdRepo = (): LoadUserByIdRepo => {
+  class LoadUserByIdRepoStub implements LoadUserByIdRepo {
+    async loadById (userId: number): Promise<UserModel> {
+      return {
+        id: 1,
+        name: 'any_name',
+        email: 'any_email'
+      }
+    }
+  }
+  return new LoadUserByIdRepoStub()
+}
+
 type SutTypes = {
   sut: DbLoadUserByToken
   decrypterStub: Decrypter
+  loadUserByIdRepoStub: LoadUserByIdRepo
 }
 
 const makeSut = (): SutTypes => {
+  const loadUserByIdRepoStub = mockLoadUserByIdRepo()
   const decrypterStub = mockDecrypter()
-  const sut = new DbLoadUserByToken(decrypterStub)
+  const sut = new DbLoadUserByToken(decrypterStub, loadUserByIdRepoStub)
   return {
     sut,
-    decrypterStub
+    decrypterStub,
+    loadUserByIdRepoStub
   }
 }
 describe('DbLoadUserByToken', () => {
@@ -36,5 +54,12 @@ describe('DbLoadUserByToken', () => {
     jest.spyOn(decrypterStub, 'decrypt').mockResolvedValueOnce(null)
     const user = await sut.load('encrypted_token')
     expect(user).toBeNull()
+  })
+
+  it('Should call LoadUserById with correct value', async () => {
+    const { sut, loadUserByIdRepoStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadUserByIdRepoStub, 'loadById')
+    await sut.load('encrypted_token')
+    expect(loadByIdSpy).toHaveBeenCalledWith(1)
   })
 })
