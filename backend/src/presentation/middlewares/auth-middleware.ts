8432a9b107eb1afe7a10dafca6b93cad6648ae2f
Renato Mendes <mendeslrenato@gmail.com>
2022-12-23 17:54:36 -0300

    feat: ensure AuthMiddleware returns server error if LoadUserByToken throws

--- backend/src/presentation/middlewares/auth-middleware.ts
+++ backend/src/presentation/middlewares/auth-middleware.ts
@@ -1,19 +1,23 @@
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 import { AccessDeniedError } from '@/presentation/errors'
-import { forbidden, ok } from '@/presentation/helpers/http-helpers'
+import { forbidden, ok, serverError } from '@/presentation/helpers/http-helpers'
 import { LoadUserByToken } from '@/domain/usecases/load-user-by-token'
 
 export class AuthMiddleware implements Controller {
   constructor (private readonly loadUserByToken: LoadUserByToken) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers?.['x-access-token']
-    if (accessToken) {
-      const user = await this.loadUserByToken.load(accessToken)
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token']
+      if (accessToken) {
+        const user = await this.loadUserByToken.load(accessToken)
 
-      if (user) return ok({ userId: user.id })
+        if (user) return ok({ userId: user.id })
+      }
+
+      return forbidden(new AccessDeniedError())
+    } catch (error) {
+      return serverError()
     }
-
-    return forbidden(new AccessDeniedError())
   }
 }
