5c083225cc92522c4f59fe8eac3a595b477a0f77
Renato Mendes <mendeslrenato@gmail.com>
2022-12-23 14:59:25 -0300

    test: ensure orders/user route returns 200 on success

---
+++ backend/tests/main/routes/orders-route.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+import Order from '@/infra/sequelize/models/Order'
+import { dbOrdersMock } from '@/tests/helpers/db-mocks'
+
+describe('GET /orders/user', () => {
+  it('Should return 200 on success', async () => {
+    jest.spyOn(Order, 'findAll').mockResolvedValueOnce(dbOrdersMock as any)
+    await request(app)
+      .get('/orders/user')
+      .send({
+        userId: 1
+      })
+      .expect(200)
+  })
+})
