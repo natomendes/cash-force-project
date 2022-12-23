516a7efaa5465bc801fe5c0bc76e60a42a9f5f24
Renato Mendes <mendeslrenato@gmail.com>
2022-12-22 21:17:47 -0300

    feat: add orders-table migration

---
+++ backend/src/infra/sequelize/migrations/20221223001101-orders-table.js
@@ -0,0 +1,122 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('orders', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      orderNfId: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      orderNumber: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      orderPath: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      orderFileName: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      orderOriginalName: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      emissionDate: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      pdfFile: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      emitedTo: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      nNf: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      CTE: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      value: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      cnpjId: {
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+        references: {
+          model: 'cnpjs',
+          key: 'id'
+        }
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      buyerId: {
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+        references: {
+          model: 'buyers',
+          key: 'id'
+        }
+      },
+      providerId: {
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+        references: {
+          model: 'providers',
+          key: 'id'
+        }
+      },
+      orderStatusBuyer: {
+        type: Sequelize.STRING,
+        defaultValue: '0'
+      },
+      orderStatusProvider: {
+        type: Sequelize.STRING,
+        defaultValue: '0'
+      },
+      deliveryReceipt: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      cargoPackingList: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      deliveryCtrc: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      }
+    })
+  },
+
+  async down (queryInterface) {
+    await queryInterface.dropTable('orders')
+  }
+}
