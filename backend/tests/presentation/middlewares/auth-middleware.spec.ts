8a8649a41eb890177b625356fa6e069ebc303696
Renato Mendes <mendeslrenato@gmail.com>
2022-12-23 17:31:04 -0300

    test: ensure AuthMiddleware calls ValidateToken with correct value

--- backend/tests/presentation/middlewares/auth-middleware.spec.ts
+++ backend/tests/presentation/middlewares/auth-middleware.spec.ts
@@ -1,15 +1,46 @@
+import { ValidateToken } from '@/domain/usecases/validate-token'
 import { AccessDeniedError } from '@/presentation/errors'
 import { forbidden } from '@/presentation/helpers/http-helpers'
 import { AuthMiddleware } from '@/presentation/middlewares'
 
-const makeSut = (): AuthMiddleware => {
-  return new AuthMiddleware()
+type SutTypes = {
+  sut: AuthMiddleware
+  validateTokenStub: ValidateToken
+}
+
+const mockValidateToken = (): ValidateToken => {
+  class ValidateTokenStub implements ValidateToken {
+    async validate (token: string): Promise<boolean> {
+      return true
+    }
+  }
+  return new ValidateTokenStub()
+}
+
+const makeSut = (): SutTypes => {
+  const validateTokenStub = mockValidateToken()
+  const sut = new AuthMiddleware(validateTokenStub)
+  return {
+    sut,
+    validateTokenStub
+  }
 }
 
 describe('Auth Middleware', () => {
   it('Should return forbidden if no x-access-token exists in headers', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  it('Should call ValidateToken with correct value', async () => {
+    const { sut, validateTokenStub } = makeSut()
+    const validateSpy = jest.spyOn(validateTokenStub, 'validate')
+    await sut.handle({
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    })
+    expect(validateSpy).toHaveBeenCalledWith('any_token')
   })
 })
