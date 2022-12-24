235a2764fa2a2bc7060cf3cd339d319a0d350ba9
Renato Mendes <mendeslrenato@gmail.com>
2022-12-24 00:09:29 -0300

    feat: ensure LoginController returns unauthorized if credentials are invalid

--- backend/src/presentation/controllers/login-controller.ts
+++ backend/src/presentation/controllers/login-controller.ts
@@ -1,5 +1,6 @@
 import { Authentication } from '@/domain/usecases/authentication'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { unauthorized } from '@/presentation/helpers/http-helpers'
 
 export class LoginController implements Controller {
   constructor (
@@ -7,7 +8,8 @@ export class LoginController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.authentication.auth(httpRequest.body)
-    return null
+    const { email, password } = httpRequest.body
+    const accessToken = await this.authentication.auth({ email, password })
+    if (!accessToken) return unauthorized()
   }
 }
--- backend/src/presentation/helpers/http-helpers.ts
+++ backend/src/presentation/helpers/http-helpers.ts
@@ -1,9 +1,14 @@
-import { ServerError } from '@/presentation/errors'
+import { ServerError, UnauthorizedError } from '@/presentation/errors'
 import { HttpResponse } from '@/presentation/protocols'
 
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
+})
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 403,
+  body: new UnauthorizedError()
 })
 
 export const forbidden = (error: Error): HttpResponse => ({
