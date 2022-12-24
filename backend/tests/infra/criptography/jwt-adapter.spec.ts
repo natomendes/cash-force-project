dcaa65008f2b749fb60f6a5e3704b954bfd88765
Renato Mendes <mendeslrenato@gmail.com>
2022-12-23 23:05:43 -0300

    test: ensure JwtAdapter calls sign with correct values

--- backend/tests/infra/criptography/jwt-adapter.spec.ts
+++ backend/tests/infra/criptography/jwt-adapter.spec.ts
@@ -2,7 +2,12 @@ import { JwtAdapter } from '@/infra/criptography/jwt-adapter'
 import { mockUserModel } from '@/tests/helpers'
 import jwt from 'jsonwebtoken'
 
+const { password, ...userWithoutPassword } = mockUserModel()
+
 jest.mock('jsonwebtoken', () => ({
+  sign (): string {
+    return 'valid_token'
+  },
   verify (): jwt.JwtPayload {
     return { user: mockUserModel() }
   }
@@ -14,27 +19,39 @@ const makeSut = (): JwtAdapter => {
 }
 
 describe('Jwt Adapter', () => {
-  it('Should call jwt.verify with correct values', async () => {
-    const sut = makeSut()
-    const verifySpy = jest.spyOn(jwt, 'verify')
-    await sut.decrypt('encrypted_token')
-    expect(verifySpy).toHaveBeenCalledWith('encrypted_token', secret)
+  describe('sign()', () => {
+    it('Should call sign with correct values', async () => {
+      const sut = makeSut()
+      const signSpy = jest.spyOn(jwt, 'sign')
+      await sut.encrypt(userWithoutPassword)
+
+      expect(signSpy).toHaveBeenCalledWith(userWithoutPassword, secret)
+    })
   })
 
-  it('Should return an user id on jwt.verify success', async () => {
-    const sut = makeSut()
+  describe('verify()', () => {
+    it('Should call jwt.verify with correct values', async () => {
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt('encrypted_token')
+      expect(verifySpy).toHaveBeenCalledWith('encrypted_token', secret)
+    })
 
-    const user = await sut.decrypt('encrypted_token')
+    it('Should return an user id on jwt.verify success', async () => {
+      const sut = makeSut()
 
-    expect(user).toBe(mockUserModel().id)
-  })
+      const user = await sut.decrypt('encrypted_token')
 
-  it('Should return null if jwt verify throws', async () => {
-    const sut = makeSut()
-    jest.spyOn(jwt, 'verify').mockImplementationOnce(() => { throw new Error() })
+      expect(user).toBe(mockUserModel().id)
+    })
 
-    const user = await sut.decrypt('encrypted_token')
+    it('Should return null if jwt verify throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => { throw new Error() })
 
-    expect(user).toBeNull()
+      const user = await sut.decrypt('encrypted_token')
+
+      expect(user).toBeNull()
+    })
   })
 })
