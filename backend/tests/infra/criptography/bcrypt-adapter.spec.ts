c1b19d1ad62c92f8616911a505ef705cc5b72482
Renato Mendes <mendeslrenato@gmail.com>
2022-12-23 22:53:11 -0300

    chore: add bcrypt

---
+++ backend/tests/infra/criptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,21 @@
+import * as bcrypt from 'bcrypt'
+import { BcryptAdapter } from '@/infra/criptography/bcrypt-adapter'
+
+jest.mock('bcrypt', () => ({
+  compare: jest.fn(async (): Promise<boolean> => await new Promise(resolve => resolve(true)))
+}))
+
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter()
+}
+
+describe('BcryptAdapter', () => {
+  describe('compare()', () => {
+    it('Should call bcrypt.compare with correct values', async () => {
+      const sut = makeSut()
+      const compareSpy = jest.spyOn(bcrypt, 'compare')
+      await sut.compare('any_password', 'hashed_password')
+      expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
+    })
+  })
+})
