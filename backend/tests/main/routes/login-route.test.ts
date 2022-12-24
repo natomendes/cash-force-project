3972b784b31c49ddf5d4e6f95a74f11dfbc4851c
Renato Mendes <mendeslrenato@gmail.com>
2022-12-24 00:50:25 -0300

    test: ensure login route returns 200 on success

---
+++ backend/tests/main/routes/login-route.test.ts
@@ -0,0 +1,21 @@
+import { mockUserModel } from '@/tests/helpers'
+import User from '@/infra/sequelize/models/User'
+import app from '@/main/config/app'
+import request from 'supertest'
+
+jest.mock('bcrypt', () => ({
+  compare: jest.fn(async (): Promise<boolean> => await new Promise(resolve => resolve(true)))
+}))
+
+describe('POST /loin', () => {
+  it('Should return 200 on login', async () => {
+    jest.spyOn(User, 'findOne').mockResolvedValueOnce(mockUserModel() as any)
+    await request(app)
+      .post('/login')
+      .send({
+        email: 'any_email@mail.com',
+        password: '123456'
+      })
+      .expect(200)
+  })
+})
