2e4922cc76af610b96f79dcab1c3e785c103e2c2
Renato Mendes <mendeslrenato@gmail.com>
2022-12-23 22:18:16 -0300

    test: ensure DbAuthentication calls HashComparer with correct values

--- backend/tests/data/usecases/db-authentication.spec.ts
+++ backend/tests/data/usecases/db-authentication.spec.ts
@@ -1,18 +1,21 @@
-import { LoadUserByEmailRepo } from '@/data/protocols'
-import { mockLoadUserByEmailRepo } from '@/tests/helpers'
+import { HashComparer, LoadUserByEmailRepo } from '@/data/protocols'
+import { mockHashComparer, mockLoadUserByEmailRepo } from '@/tests/helpers'
 import { DbAuthentication } from '@/data/usecases'
 
 type SutTypes = {
   sut: DbAuthentication
   loadUserByEmailRepoStub: LoadUserByEmailRepo
+  hashComparerStub: HashComparer
 }
 
 const makeSut = (): SutTypes => {
   const loadUserByEmailRepoStub = mockLoadUserByEmailRepo()
-  const sut = new DbAuthentication(loadUserByEmailRepoStub)
+  const hashComparerStub = mockHashComparer()
+  const sut = new DbAuthentication(loadUserByEmailRepoStub, hashComparerStub)
   return {
     sut,
-    loadUserByEmailRepoStub
+    loadUserByEmailRepoStub,
+    hashComparerStub
   }
 }
 
@@ -36,5 +39,26 @@ describe('DbAuthentication', () => {
       password: 'any_password'
     })
     await expect(promise).rejects.toThrow(new Error())
+  })
+
+  it('Should return null if LoadAccountByEmailRepository return null', async () => {
+    const { sut, loadUserByEmailRepoStub } = makeSut()
+    jest.spyOn(loadUserByEmailRepoStub, 'loadByEmail')
+      .mockResolvedValueOnce(null)
+    const accessToken = await sut.auth({
+      email: 'any_email',
+      password: 'any_password'
+    })
+    expect(accessToken).toBeNull()
+  })
+
+  it('Should call HashComparer with correct values', async () => {
+    const { sut, hashComparerStub } = makeSut()
+    const compareSpy = jest.spyOn(hashComparerStub, 'compare')
+    await sut.auth({
+      email: 'any_email',
+      password: 'any_password'
+    })
+    expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
   })
 })
