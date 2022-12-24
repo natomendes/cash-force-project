9336d94eb33b2af43444625bbabba2ccefb57d94
Renato Mendes <mendeslrenato@gmail.com>
2022-12-23 22:42:19 -0300

    feat: ensure LoadUserByEmail returns a user on succes

--- backend/src/infra/db/mysql/user-repository.ts
+++ backend/src/infra/db/mysql/user-repository.ts
@@ -1,9 +1,21 @@
-import { LoadUserByIdRepo, UserModel } from '@/data/protocols'
+import { LoadUserByEmailRepo, LoadUserByIdRepo, UserModel } from '@/data/protocols'
 import User from '@/infra/sequelize/models/User'
 
-export class UserMySqlRepository implements LoadUserByIdRepo {
+export class UserMySqlRepository implements LoadUserByIdRepo, LoadUserByEmailRepo {
   async loadById (userId: number): Promise<UserModel> {
     const user = await User.findByPk(userId)
+    if (!user) return null
+
+    const { id, username, email, password } = user
+    return { id, username, email, password }
+  }
+
+  async loadByEmail (userEmail: string): Promise<UserModel> {
+    const user = await User.findOne({
+      where: {
+        email: userEmail
+      }
+    })
     if (!user) return null
 
     const { id, username, email, password } = user
