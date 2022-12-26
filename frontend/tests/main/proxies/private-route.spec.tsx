ea8952185070dd460fd8fce112b9dc73e4cf9ad5
Renato Mendes <mendeslrenato@gmail.com>
2022-12-26 13:45:45 -0300

    test: ensure PrivateRoute renders component if token is found

--- frontend/tests/main/proxies/private-route.spec.tsx
+++ frontend/tests/main/proxies/private-route.spec.tsx
@@ -1,32 +1,40 @@
-import React from 'react'
 import { renderWithRouter } from '@/tests/presentation/pages/renderWithRouter'
+import { ApiContext } from '@/presentation/contexts'
 import PrivateRoute from '@/main/proxies/private-route'
 import { Route, Routes } from 'react-router-dom'
-import 'jest-localstorage-mock'
 import { RenderResult } from '@testing-library/react'
+import 'jest-localstorage-mock'
+import React from 'react'
 
 describe('PrivateRoute', () => {
   beforeEach(() => {
     localStorage.clear()
   })
 
-  const makeSut = (): RenderResult => {
+  const makeSut = (accessToken = 'any_token'): RenderResult => {
     return renderWithRouter(
-      <Routes>
-        <Route path="/" element={
-          <PrivateRoute>
-            <div>Private Route Mock</div>
-          </PrivateRoute>
-        } />
-        <Route path="/login" element={
-          <div>Login Mock</div>
-        }/>
-      </Routes>
+      <ApiContext.Provider value={{ getAccessToken: () => accessToken }} >
+        <Routes>
+          <Route path="/" element={
+            <PrivateRoute>
+              <div>Private Route Mock</div>
+            </PrivateRoute>
+          } />
+          <Route path="/login" element={
+            <div>Login Mock</div>
+          }/>
+        </Routes>
+      </ApiContext.Provider>
     )
   }
 
   it('Should redirect to /login if accessToken is empty', () => {
-    makeSut()
+    makeSut(null)
     expect(window.location.pathname).toBe('/login')
+  })
+
+  it('Should go to correct path if accessToken is found', () => {
+    makeSut()
+    expect(window.location.pathname).toBe('/')
   })
 })
