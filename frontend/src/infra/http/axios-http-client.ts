2cfc653286cba0e4bb10581af28ac3c6bf6da16f
Renato Mendes <mendeslrenato@gmail.com>
2022-12-26 19:26:20 -0300

    feat: ensure HttpGetClient calls axios with correct values

--- frontend/src/infra/http/axios-http-client.ts
+++ frontend/src/infra/http/axios-http-client.ts
@@ -1,11 +1,24 @@
-import { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http'
+import { HttpGetClient, HttpGetParams, HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http'
 import axios, { AxiosResponse } from 'axios'
 
-export class AxiosHttpClient implements HttpPostClient<any> {
+export class AxiosHttpClient implements HttpPostClient<any>, HttpGetClient<any> {
   async post ({ url, body }: HttpPostParams): Promise<HttpResponse<any>> {
     let httpResponse: AxiosResponse<any>
     try {
       httpResponse = await axios.post(url, body)
+    } catch (error) {
+      httpResponse = error.response
+    }
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
+  }
+
+  async get ({ url, headers }: HttpGetParams): Promise<HttpResponse<any>> {
+    let httpResponse: AxiosResponse<any>
+    try {
+      httpResponse = await axios.get(url, { headers })
     } catch (error) {
       httpResponse = error.response
     }
