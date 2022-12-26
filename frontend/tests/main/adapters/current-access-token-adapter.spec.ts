f6894cd72354b6bce84cc445aaacb061ee57e579
Renato Mendes <mendeslrenato@gmail.com>
2022-12-26 12:29:08 -0300

    test: ensure CurrentAccessTokenAdapter returns an accessToken on LocalStorageAdapter.get success

--- frontend/tests/main/adapters/current-access-token-adapter.spec.ts
+++ frontend/tests/main/adapters/current-access-token-adapter.spec.ts
@@ -1,11 +1,23 @@
 import { LocalStorageAdapter } from '@/infra/cache'
-import { setCurrentTokenAdapter } from '@/main/adapters/current-token-adapter'
+import { setCurrentTokenAdapter, getCurrentTokenAdapter } from '@/main/adapters/current-token-adapter'
 import 'jest-localstorage-mock'
 
 describe('CurrentAccessTokenAdapter', () => {
-  it('Should call LocalStorageAdapter with correct values', () => {
+  it('Should call LocalStorageAdapter.set with correct values', () => {
     const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
     setCurrentTokenAdapter('any_token')
     expect(setSpy).toHaveBeenCalledWith('accessToken', 'any_token')
+  })
+
+  it('Should call LocalStorageAdapter.get with correct value', () => {
+    const getSpy = jest.spyOn(LocalStorageAdapter.prototype, 'get').mockReturnValueOnce('any_token')
+    getCurrentTokenAdapter()
+    expect(getSpy).toHaveBeenCalledWith('accessToken')
+  })
+
+  it('Should return an accessToken on LocalStorageAdapter.get success', () => {
+    jest.spyOn(LocalStorageAdapter.prototype, 'get').mockReturnValueOnce('any_token')
+    const accessToken = getCurrentTokenAdapter()
+    expect(accessToken).toBe('any_token')
   })
 })
