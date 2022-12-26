ef1fff2407fd3fa9db05cd178f600486a059137a
Renato Mendes <mendeslrenato@gmail.com>
2022-12-26 12:28:46 -0300

    feat: ensure CurrentAccessTokenAdapter returns an accessToken on LocalStorageAdapter.get success

--- frontend/src/main/adapters/current-token-adapter.ts
+++ frontend/src/main/adapters/current-token-adapter.ts
@@ -3,3 +3,7 @@ import { makeLocalStorageAdapter } from '@/main/factories/usecases/cache'
 export const setCurrentTokenAdapter = (accessToken: string): void => {
   makeLocalStorageAdapter().set('accessToken', accessToken)
 }
+
+export const getCurrentTokenAdapter = (): string => {
+  return makeLocalStorageAdapter().get('accessToken')
+}
