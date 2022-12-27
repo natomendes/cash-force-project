227c50ca434772c06fb04c8ffd694ec8ddcde9ae
Renato Mendes <mendeslrenato@gmail.com>
2022-12-26 22:05:27 -0300

    feat: add Dashboard Context

--- frontend/src/presentation/contexts/index.ts
+++ frontend/src/presentation/contexts/index.ts
@@ -1,2 +1,3 @@
 export { default as ApiContext } from './api-context'
 export { default as FormContext } from './form-context'
+export { default as DashboardContext } from './dashboard-context'
--- frontend/src/presentation/pages/dashboard/dashboard.tsx
+++ frontend/src/presentation/pages/dashboard/dashboard.tsx
@@ -3,18 +3,20 @@ import React from 'react'
 import { DashboardContent, Sider } from '@/presentation/components'
 import { OrderModel } from '@/domain/models'
 import { useLoaderData } from 'react-router-dom'
+import { DashboardContext } from '@/presentation/contexts'
 
 const Dashboard: React.FC = () => {
   const orders = useLoaderData() as OrderModel[]
-  console.log(orders)
   return (
-    <div className={Styles.dashboard}>
-      <Sider />
-      <div className={Styles.main}>
-        <div className={Styles.header} />
-        <DashboardContent />
+    <DashboardContext.Provider value={{ orders }}>
+      <div className={Styles.dashboard}>
+        <Sider />
+        <div className={Styles.main}>
+          <div className={Styles.header} />
+          <DashboardContent />
+        </div>
       </div>
-    </div>
+    </DashboardContext.Provider>
   )
 }
 
