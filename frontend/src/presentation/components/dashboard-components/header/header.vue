f7c8404dbdd6c37def932da58b0117ae76dfc6d4
Renato Mendes <mendeslrenato@gmail.com>
2022-12-29 17:10:56 -0300

    feat: add logout button to dashboard page

--- frontend/src/presentation/components/dashboard-components/header/header.vue
+++ frontend/src/presentation/components/dashboard-components/header/header.vue
@@ -4,17 +4,25 @@
       :src="logoutIcon"
       alt="logout icon"
       title="logout"
+      @click="logout"
     >
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import { apiStore } from '@/presentation/store/api-store'
 
 export default defineComponent({
   data: () => ({
     logoutIcon: 'assets/icons/logout.svg'
-  })
+  }),
+  methods: {
+    logout () {
+      apiStore.clearCurrentAccount()
+      this.$router.push({ path: '/login', replace: true })
+    }
+  }
 })
 </script>
 
